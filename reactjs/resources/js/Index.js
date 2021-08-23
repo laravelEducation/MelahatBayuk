6a2492459f931402d18f6bbee1d5cb8280dfca04
MelahatBayuk <70443583+MelahatBayuk@users.noreply.github.com>
2021-08-23 15:15:19 +0300

    initial commit

---
+++ reactjs/resources/js/Index.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+class Index extends Component{
+    render() {
+        return(<div>Merhaba</div>)
+    }
+}
+ReactDOM.render(<Index/>,document.getElementById('index'));
---
+++ reactjs/resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Laravel 8 & React JS</title>
+</head>
+<body>
+        <div id="index"></div>
+        <script src="{{asset('js/app.js')}}"></script>
+</body>
+
+</html>
